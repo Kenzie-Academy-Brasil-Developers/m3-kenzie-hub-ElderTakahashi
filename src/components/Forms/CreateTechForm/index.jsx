efe14d7f039a8078a828803c043c7222e40cc1ce
Elder Toshiro Takahashi <eldertaka@gmail.com>
2023-08-24 13:04:52 -0300

    typo corrected

--- src/components/Forms/CreateTechForm/index.jsx
+++ src/components/Forms/CreateTechForm/index.jsx
@@ -1,6 +1,6 @@
 import { Input } from "../Input";
 import { Select } from "../Select";
-import { set, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useContext, useState } from "react";
 import { createTechFormSchema } from "./createTechFormSchema";
